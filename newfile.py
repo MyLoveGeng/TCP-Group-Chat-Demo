753d180e1cbd9035596edeb1ce57385532021505
MyLoveGeng <my_love.geng@foxmail.com>
2019-09-19 22:06:49 +0800

    使用类文件对象改写源文件<makefile>

--- newfile.py
+++ newfile.py
@@ -22,39 +22,42 @@ class ChatServer:
     def accept(self):
         while True:  # 一个线程
             s, raddr = self.sock.accept()  # 阻塞
+            f = s.makefile(mode='rw')
             logging.info(raddr)
             logging.info(s)
-            self.clients[raddr] = s
-            threading.Thread(target=self.recv, name='recv', args=(s, raddr)).start()
+            logging.info(f)
+            self.clients[raddr] = f
+            threading.Thread(target=self.recv, name='recv', args=(f, raddr)).start()
 
-    def recv(self, sock, addr):
+    def recv(self, f, addr):
         while not self.event.is_set():
             try:
-                data = sock.recv(1024)  # 阻塞, bytes
-                if data == b'':
+                # data = f.recv(1024)  # 阻塞, bytes\
+                data = f.readline()  # string, 有换行符
+                if data == '':
                     # python下的socket不能检查连接状态，当断开连接时，recv内容为空
                     raise Exception()
                 logging.info(data)
-                logging.info('recv')
             except Exception as e:
                 logging.error(e)
-                data = b'quit'
-            if data == b'quit':
+                data = 'quit'
+            if data == 'quit':
                 # 推出群聊
-                sock.close()
+                f.close()
                 self.clients.pop(addr)
                 logging.info(addr)
                 logging.info("已退出")
                 break
             msg = "{} {} {}".format(
-                sock.getpeername(),
+                addr,
                 datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S"),
-                data.decode('gb2312')
-            ).encode('gb2312')
-            for s in self.clients.values():
-                if s == sock:
+                data
+            )
+            for s in self.clients.keys():
+                if s == addr:
                     continue
-                s.send(msg)
+                f.write()
+                f.flush()
 
     def stop(self):
         for s in self.clients.values():
