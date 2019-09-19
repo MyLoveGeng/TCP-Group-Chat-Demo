89aa02797a5c8b6f52715b50581ec70f4e386641
MyLoveGeng <my_love.geng@foxmail.com>
2019-09-19 21:32:58 +0800

    添加群聊，处理断开连接

--- newfile.py
+++ newfile.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import logging
+import datetime
 
 FORMAT = "%(asctime)s %(threadName)s %(thread)d %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO)
@@ -11,6 +12,7 @@ class ChatServer:
     def __init__(self, ip='192.168.1.100', port=9999):
         self.addr = (ip, port)
         self.sock = socket.socket()
+        self.clients = {}
 
     def start(self):
         self.sock.bind(self.addr)
@@ -18,19 +20,45 @@ class ChatServer:
         threading.Thread(target=self.accept, name='accept').start()  # 给其他线程，保证主线程不阻塞
 
     def accept(self):
-        while True:
+        while True:  # 一个线程
             s, raddr = self.sock.accept()  # 阻塞
             logging.info(raddr)
             logging.info(s)
+            self.clients[raddr] = s
             threading.Thread(target=self.recv, name='recv', args=(s, raddr)).start()
 
     def recv(self, sock, addr):
         while True:
-            data = sock.recv(1024)  # 阻塞, bytes
-            logging.info(data)
-            sock.send('ack '.format(data.decode()).encode())
+            try:
+                data = sock.recv(1024)  # 阻塞, bytes
+                if data == b'':
+                    # python 不支持状态检测，当断开socket时，recv为空
+                    raise Exception()
+                logging.info(data)
+                logging.info('recv')
+            except Exception as e:
+                logging.error(e)
+                data = b'quit'
+            if data == b'quit':
+                # 推出群聊
+                sock.close()
+                self.clients.pop(addr)
+                logging.info(addr)
+                logging.info("已退出")
+                break
+            msg = "{} {} {}".format(
+                sock.getpeername(),
+                datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S"),
+                data.decode('gb2312')
+            ).encode('gb2312')
+            for s in self.clients.values():
+                if s == sock:
+                    continue
+                s.send(msg)
 
     def stop(self):
+        for s in self.clients.values():
+            s.close()
         self.sock.close()
 
 cs = ChatServer()
@@ -39,5 +67,7 @@ cs.start()
 while True:
     cmd = input(">>>")
     if cmd.strip() == 'quit':
+        cs.stop()
+        threading.Event.wait(3)
         break
     logging.info(threading.enumerate())
