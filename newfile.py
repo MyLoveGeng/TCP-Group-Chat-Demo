74ca214f794129742e4cf85c61b71d8d2485e843
MyLoveGeng <my_love.geng@foxmail.com>
2019-09-19 16:38:01 +0800

    TCP Group Chat Demo Starts!!

---
+++ newfile.py
@@ -0,0 +1,16 @@
+import socket
+import threading
+import logging
+
+FORMAT = "%(asctime)s %(threadName)s %(thread)d %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+
+class ChatServer:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
